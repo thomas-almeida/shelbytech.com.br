591a6ad48375144b93377747f32fa691223c2663
thomas almeida <thomas.rodrigues@idealtrends.com.br>
2023-01-12 05:50:28 -0300

    links navbar e notion

--- inc/navbar.php
+++ inc/navbar.php
@@ -6,7 +6,7 @@
             <li class="hide-mobile" onclick="activateMessage()">Por que nós?</li>
             <li class="hide-mobile"> <a target="_blank" href="https://www.instagram.com/shelbytech.com.br/">Social</a></li>
             <li class="hide-mobile"><a target="_blank" href="https://www.tabnews.com.br/thomalmeida">Blog</a></li>
-            <li class="hide-mobile">Podcast (breve)</li>
+            <li class="hide-mobile"><a target="_blank" href="https://awesome-appeal-0eb.notion.site/Shelby-Tech-6cc7c2471e284359a788a0b830a5c92c">Conheça a Shelby Tech?</a></li>
             </li>
         </ul>
     </div>
